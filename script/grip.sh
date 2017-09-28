622d9e33d086b45a7c62ed747de53d6f7edf5fea
@slovacus <slovacus@gmail.com>
2017-09-28 10:51:59 -0500

    fix: clean unnecessary script (#84)

--- script/grip.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-
-# shellcheck source=script/bootstrap.sh
-[ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
-
-grip --pass "${GITHUB_API_TOKEN}" ${GRIP_PORT}
--- script/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-
-# shellcheck source=script/bootstrap.sh
-[ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
-
-pip install -r "${REQUIREMENTS_DIR}/dev.txt"
