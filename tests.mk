979552381a99bcaff93b2762ca0bf8f40e67a658
@slovacus <slovacus@gmail.com>
2017-11-21 22:25:38 -0500

    chore: actions for test for make (#106)

--- tests.mk
+++ tests.mk
@@ -2,9 +2,23 @@
 # See ./CONTRIBUTING.rst
 #
 
+DOCKER_TEST := docker-compose -f docker-compose.yml -f docker-compose/test.yml
+
+test: clean
+	@echo $(MESSAGE) Running tests on the current Python interpreter with coverage $(END)
+	$(DOCKER_TEST) run --rm app \
+		bash -c "py.test --cov lannister --cov tests --doctest-modules --verbose lannister tests";
+	@echo
+
 test.lint: clean
 	pre-commit run --all-files --verbose
 
-test.syntax: clean
-	@echo $(MESSAGE) Running tests $(END)
-	ansible-playbook tests/test.yaml -i tests/inventory --syntax-check
+test.pytest: clean
+	@echo $(MESSAGE) Running tests on the current Python $(END)
+	@if [ "${test}" == "" ]; then \
+		$(DOCKER_TEST) run --rm app \
+			bash -c "py.test -s lannister tests"; \
+	else \
+		$(DOCKER_TEST) run --rm app \
+			bash -c "py.test -s -v tests/${test}"; \
+	fi
