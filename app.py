e2e63873c959b7c02dea3b27765e47b54bae7ae9
@slovacus <slovacus@gmail.com>
2017-11-23 00:17:01 -0500

    fix: change bind all ip (#106)

--- app.py
+++ app.py
@@ -30,3 +30,7 @@ async def webhook(request):
         for message in entry['messaging']:
             return json({'response': '{} o/'.format(message)})
     return json({'response': 'o/'})
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000)
