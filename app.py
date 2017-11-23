1dbc5e7472e877590b915f00d465bea4216dfd97
@slovacus <slovacus@gmail.com>
2017-11-23 01:20:42 -0500

    fix: clean unnecessary run in app (#106)

--- app.py
+++ app.py
@@ -30,7 +30,3 @@ async def webhook(request):
         for message in entry['messaging']:
             return json({'response': '{} o/'.format(message)})
     return json({'response': 'o/'})
-
-
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000)
